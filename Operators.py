3c09c9b9089b8c183667418fbd93ed7126f5fd79
MauoodHamidi <52146596+MauoodHamidi@users.noreply.github.com>
2019-07-18 22:40:31 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,100 @@
+#Arithmatic Operators
+a=19
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+#Comparison Operatots
+print(a>b)
+print(a>=b)
+print(a<b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+#Logical Operators
+c=True
+d=False
+print(c and d)
+print(c or d)
+print(not c)
+print(not d)
+
+#Bitwise Operators
+e=10
+f=4
+print(e & f)
+print(e | f)
+print(e^f)
+print(~e)
+print(e>>f)
+print(e<<f)
+
+#Assignment operators
+g=10
+print(g)
+g+=5
+print(g)
+g-=5
+print(g)
+g*=5
+print(g)
+g/=5
+print(g)
+g%=5
+print(g)
+g//=5
+print(g)
+g**=5
+print(g)
+ig=10
+ig&=5
+print(ig)
+ig|=5
+print(ig)
+ig^=5
+print(ig)
+ig>>=5
+print(ig)
+ig<<=5
+print(ig)
+#Special Operators
+x="LokeshIT"
+print(x)
+print('p' in x)
+print('p' not in x)
+print('k' in x)
+print('k' not in x)
+
+#Identity Operators
+#Firts Scenario
+h=1000
+i=2000
+print(h==i)
+print(h!=i)
+print(h is i)
+print(h is not i)
+#Second Scenario
+j=3000
+k=3000
+print(j==k)
+print(j!=k)
+print(j is k)
+print(j is not k)
+#Third Scenario
+p=[10,20,30]
+q=[40,50,60]
+print(p==q)
+print(p!=q)
+print(p is q)
+print(p is not q)
+#Fourth Scenario
+x=[70,80,90]
+y=[70,80,90]
+print(x==y)
+print(x!=y)
+print(x is y)
+print(x is not y)
---
+++ Type Conversion.py
@@ -0,0 +1,15 @@
+x="LokeshIT"
+print(x)
+y="Python"
+print(y)
+z="1234"
+print(z)
+p=1000
+print(p)
+q=2000
+print(q)
+print(x+y)
+print(x+z)
+print(x+str(p))
+print(int(z)+p)
+print(z+str(p))
